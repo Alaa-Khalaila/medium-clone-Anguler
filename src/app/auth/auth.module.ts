1e75cff49a0ade623fcf9142e3bb29881865027b
Alaa-Khalila <alkhalila.alaa@gmail.com>
2021-11-14 15:30:38 +0200

    initial commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    RegisterComponent
+  ],
+})
+export class AuthModule {}
